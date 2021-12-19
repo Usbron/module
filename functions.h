de50833ad4037a2ead7068628566e8a30a36bcb7
Usbron <91209224+Usbron@users.noreply.github.com>
2021-12-20 00:04:37 +0300

    Delete functions.h

--- functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void ex1(double a, double c, double x);
-void ex2(double t);
-void ex3(double a, double b, double c);
-void ex4(int c, double t);
-void ex5();
-void ex6(int* arr, int n);
-void ex7(int I, int J);
